76cbefa434d4e9fa4ff4c6e9215eea6a0012f5b2
Geo <gnicholsoncloud@gmail.com>
2019-04-22 22:18:14 +0100

    Polished Update
    -Added writing to text file
    -Added indicator of speech

--- Assets/Scripts/VoiceRecognitionScript.cs
+++ Assets/Scripts/VoiceRecognitionScript.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.Windows.Speech;
+using UnityEngine.UI;
+using System.IO;
 
 public class VoiceRecognitionScript : MonoBehaviour
 {
     private DictationRecognizer dictationRecognizer;
     public string[] keywords = new string[] { "fire", "ice", "confuse", "shield" };
+    private string heldString;
+    public TextAsset asset;
+    public Button button;
 
     void Start()
     {
@@ -21,7 +27,7 @@ public class VoiceRecognitionScript : MonoBehaviour
 
     void Update ()
     {
-        if(UIScript.hypothesis != null)
+        if (UIScript.hypothesis != null)
         {
             foreach (string i in keywords)
             {
@@ -30,7 +36,29 @@ public class VoiceRecognitionScript : MonoBehaviour
                     UIScript.spell = i;
                 }
             }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                WriteString();
+
+            if(UIScript.hypothesis != heldString)
+                button.GetComponent<Image>().color = Color.red;
         }
-  
+
+        
+    }
+
+ 
+    void WriteString()
+    {
+        heldString = UIScript.hypothesis;
+
+        button.GetComponent<Image>().color = Color.green;
+        string path = "Assets/ListOfWords.txt";
+
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(heldString);
+        writer.Close();
+
     }
 }
